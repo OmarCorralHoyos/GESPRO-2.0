1fcd6cd9f0dc95c0ed68d7c83395892fc91afa3e
OmarCorralHoyos <o.corral.h@gmail.com>
2022-06-14 06:22:55 -0500

    actualizacion

---
+++ database/db.js
@@ -0,0 +1,20 @@
+/* Conexion base de datos */ 
+
+const mysql = require("mysql");
+
+const conexion = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'Mila081316',
+    database:'gespro' 
+});
+
+conexion.connect((error)=>{
+    if(error){
+        console.error('El error de conexion es: '+error);
+        return
+    }
+    console.log('¡Conectado a la db gespro del proyecto!');
+})
+
+module.exports=conexion
